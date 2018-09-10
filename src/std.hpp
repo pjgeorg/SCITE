9534e6b0d93ff11a3ac9e2faee1dbb6599776d81
Peter Georg <peter.georg@physik.uni-regensburg.de>
2018-09-10 13:25:07 +0200

    C++17 is required

--- src/std.hpp
+++ src/std.hpp
@@ -11,7 +11,7 @@
 #ifndef __cplusplus
 #error A C++ compiler is required. To disable compiler checks, define NO_COMPILER_CHECKS.
 #elif __cplusplus < 201703L
-#error A C++14 compiler is required. To disable compiler checks, define NO_COMPILER_CHECKS.
+#error A C++17 compiler is required. To disable compiler checks, define NO_COMPILER_CHECKS.
 #endif // __cplusplus
 
 #endif // STD_H
